15493bbc4a2e1ba1854d72debaba2de17f817b5f
rickslick3 <richard.robertsf@gmail.com>
2025-03-07 14:42:40 -0500

    Starter code from class

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+d3.csv('data/2024-2025.csv')  //**** TO DO  switch this to loading the quakes 'data/2024-2025.csv'
+.then(data => {
+    console.log("number of items: " + data.length);
+
+    data.forEach(d => {  //convert from string to number
+      d.latitude = +d.latitude; 
+      d.longitude = +d.longitude;  
+    });
+
+    // const subsetData = data.filter(d => d.magSource == "us");
+    // const subsetData = data.filter((d, i) => i < 500);
+    const subsetData = data.filter((d, i) => d.type == "earthquake" && i < 3000)
+
+    // Initialize chart and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, subsetData);
+
+
+  })
+  .catch(error => console.error(error));
